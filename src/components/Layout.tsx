00a306a64778387bb6972d6cca7b7a1ca8194f9b
Marvin <maples4000@googlemail.com>
2024-06-18 10:45:44 +0200

    Deleted unnecessary Div elements

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,23 +6,17 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
     return (
         <div>
-            <div>
-                <header className="pokacid">Pokacid</header>
-                <aside>
-                    <nav>
-                        <div className="navbar">
-                            <Link href="/">Go to Homepage</Link>
-                            <Link href="/pokedex-site">Watch your Pokedex</Link>
-                        </div>
-                    </nav>
-                </aside>
-            </div>
-            <div>
-                <main>{children}</main>
-            </div>
-            <div>
-                <footer className="footer">About me</footer>
-            </div>
+            <header className="pokacid">Pokacid</header>
+            <aside>
+                <nav>
+                    <div className="navbar">
+                        <Link href="/">Go to Homepage</Link>
+                        <Link href="/pokedex-site">Watch your Pokedex</Link>
+                    </div>
+                </nav>
+            </aside>
+            <main>{children}</main>
+            <footer className="footer">About me</footer>
         </div>
     );
 }
