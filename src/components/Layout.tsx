3f4a0b57f33f5eecea1a21eda3acc22787b9ce04
Marvin <maples4000@googlemail.com>
2024-06-21 15:03:04 +0200

    Implemented a hoverable Pokeball to open the Sidebar.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,40 @@
 import Link from "next/link";
+import { useState } from "react";
 
 type LayoutProps = {
     children: React.ReactNode;
 };
 export default function Layout({ children }: LayoutProps) {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <>
-            <header className="pokacid">Pokacid</header>
-            <aside>
-                <nav className="navbar">
-                    <Link href="/">Go to Homepage</Link>
-                    <Link href="/pokedex-site">Watch your Pokedex</Link>
-                </nav>
-            </aside>
-            <main>{children}</main>
-            <footer className="footer">About me</footer>
-        </>
+        <div className="wrapper">
+            <header className="header">
+                <div
+                    onMouseEnter={() => setIsOpen(true)}
+                    onMouseLeave={() => setIsOpen(false)}
+                    className="pokeball"
+                ></div>
+                <div className="pokacid">Pokacid</div>
+                <button className="themebutton">Light/Dark</button>
+            </header>
+            <div className="bodywrapper">
+                {isOpen && (
+                    <aside
+                        onMouseEnter={() => setIsOpen(true)}
+                        onMouseLeave={() => setIsOpen(false)}
+                        className="sidebar"
+                    >
+                        <nav className="navbar">
+                            <Link href="/">Go to Homepage</Link>
+                            <Link href="/pokedex-site">Watch your Pokedex</Link>
+                        </nav>
+                    </aside>
+                )}
+                <div className="body">
+                    <main className="main">{children}</main>
+                    <footer className="footer">About me</footer>
+                </div>
+            </div>
+        </div>
     );
 }
