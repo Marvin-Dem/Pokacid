f21af32c4836fb76c8ec90e6b795c8eb8cde4ede
Marvin <165769015+Marvin-Dem@users.noreply.github.com>
2024-10-14 16:19:10 +0200

    feat(#23): pokelist redesign

--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -1,48 +1,69 @@
+import Image from "next/image";
 import type { Pokemon } from "pokenode-ts";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
+import { getBackgroundColor } from "~/pages/pokedex-site";
+import { Type } from "~/utils/pokeTypes";
 
 type PokemonCardProps = {
     pokemon: Pokemon;
+    onMouseEnter: () => void;
+    onMouseLeave: () => void;
 };
-export default function PokemonCard({ pokemon }: PokemonCardProps) {
+export default function PokemonCard({
+    pokemon,
+    onMouseEnter,
+    onMouseLeave,
+}: PokemonCardProps) {
     const audioRef = useRef<HTMLAudioElement>(null);
+    const [listVisible, setListVisible] = useState(false);
+
+    useEffect(() => {
+        setListVisible(true);
+    }, []);
 
     return (
-        <div className={`general ${pokemon.types[0]!.type.name}`}>
-            <a className="poke-name" href={`/detailedpokemon/${pokemon.id}`}>
-                {pokemon.name}
-            </a>
-            <a href={`/detailedpokemon/${pokemon.id}`}>
-                <img src={pokemon.sprites.front_default!} />
-            </a>
-            <div className="inner-card-wrapper">
-                <div className="poke-card-wrap-1">
-                    <button
-                        className="audio-button"
-                        onClick={() => {
-                            audioRef.current?.play();
-                        }}
-                    >
-                        <img
-                            src="https://cdn-icons-png.flaticon.com/512/4028/4028535.png"
-                            className="audio-button"
-                        />
-                        <audio
-                            ref={audioRef}
-                            src={pokemon.cries.legacy}
-                        ></audio>
-                    </button>
-                    <span className="poke-number">{pokemon.id}</span>
-                </div>
-                <div className="poke-card-wrap-2">
-                    {pokemon.types.map((type) => {
-                        return (
-                            <span className="poke-type" key={type.type.name}>
-                                {type.type.name}
-                            </span>
-                        );
-                    })}
-                </div>
+        <div
+            className={`${getBackgroundColor(
+                pokemon.types[0]!.type.name as Type
+            )} items-center justify-between p-1.5 flex border-2 border-black rounded-3xl transition-all duration-500 ${
+                listVisible ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"
+            }`}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+        >
+            <div className="flex items-center gap-4 font-bold text-5xl">
+                <span className="w-[4ch] text-center">{pokemon.id}</span>
+                <a href={`/detailedpokemon/${pokemon.id}`} className="w-[15ch]">
+                    {pokemon.name}
+                </a>
+            </div>
+            {/* Ensures that there is always enough space for two types. */}
+            <div className="flex flex-col h-[72px]">
+                {pokemon.types.map((type) => {
+                    return (
+                        <span
+                            className="text-3xl w-[8ch] font-semibold"
+                            key={type.type.name}
+                        >
+                            {type.type.name}
+                        </span>
+                    );
+                })}
+            </div>
+            <div className="rounded-full h-[72px] w-[72px]">
+                <button
+                    onClick={() => {
+                        audioRef.current?.play();
+                    }}
+                >
+                    <Image
+                        src="/PlayButton.png"
+                        alt="audio button"
+                        width={96}
+                        height={96}
+                    />
+                    <audio ref={audioRef} src={pokemon.cries.legacy}></audio>
+                </button>
             </div>
         </div>
     );
--- src/pages/pokedex-site.tsx
+++ src/pages/pokedex-site.tsx
@@ -4,10 +4,53 @@ import { getAllPokemon } from "~/utils/pokeAPI";
 import { useEffect, useState } from "react";
 import { Pokemon } from "pokenode-ts";
 import Layout from "~/components/Layout";
+import Image from "next/image";
+
+export function getBackgroundColor(pokeType: Type) {
+    if (pokeType === "water") {
+        return "bg-water";
+    } else if (pokeType === "fire") {
+        return "bg-fire";
+    } else if (pokeType === "grass") {
+        return "bg-grass";
+    } else if (pokeType === "normal") {
+        return "bg-normal";
+    } else if (pokeType === "rock") {
+        return "bg-rock";
+    } else if (pokeType === "ground") {
+        return "bg-ground";
+    } else if (pokeType === "electric") {
+        return "bg-electric";
+    } else if (pokeType === "flying") {
+        return "bg-flying";
+    } else if (pokeType === "ice") {
+        return "bg-ice";
+    } else if (pokeType === "dragon") {
+        return "bg-dragon";
+    } else if (pokeType === "ghost") {
+        return "bg-ghost";
+    } else if (pokeType === "psychic") {
+        return "bg-psychic";
+    } else if (pokeType === "poison") {
+        return "bg-poison";
+    } else if (pokeType === "bug") {
+        return "bg-bug";
+    } else if (pokeType === "steel") {
+        return "bg-steel";
+    } else if (pokeType === "fairy") {
+        return "bg-fairy";
+    } else if (pokeType === "fighting") {
+        return "bg-fighting";
+    } else if (pokeType === "dark") {
+        return "bg-dark";
+    }
+}
 
 export default function PokedexSite() {
     const [allPokemon, setAllPokemon] = useState<Pokemon[]>([]);
     const [pokemonType, setPokemonType] = useState<Type>();
+    const [spriteImage, setSpriteImage] = useState<string>();
+    const [showTypeBox, setShowTypeBox] = useState<boolean>(false);
 
     let filteredList: Pokemon[];
     if (pokemonType === undefined) {
@@ -24,36 +67,86 @@ export default function PokedexSite() {
 
     return (
         <Layout>
-            <p className="dex-header-text">
-                Show Pokemon with the following type:
-            </p>
-            <button
-                className="button reset"
-                onClick={() => {
-                    setPokemonType(undefined);
-                }}
-            >
-                Reset Filter
-            </button>
-            <div className="button-wrapper">
-                {buttonTypes.map((buttonType) => {
-                    return (
-                        <button
-                            key={buttonType}
-                            className={`button ${buttonType} filter-button `}
-                            onClick={() => {
-                                setPokemonType(buttonType);
-                            }}
-                        >
-                            {buttonType}
-                        </button>
-                    );
-                })}
-            </div>
-            <div id="pokemon-card-wrapper">
-                {filteredList.map((pokemon) => {
-                    return <PokemonCard key={pokemon.name} pokemon={pokemon} />;
-                })}
+            <div className="grid grid-cols-3 gap-2 gap-x-12">
+                <div className="flex flex-col gap-2">
+                    <button
+                        className="text-3xl font-bold p-4 text-white border-none rounded-3xl duration-300 bg-gray-500 text-shadow"
+                        onClick={() => {
+                            setShowTypeBox(!showTypeBox);
+                        }}
+                    >
+                        Show pokemon with the following type:
+                    </button>
+                    <button
+                        className="p-4 text-3xl text-white border-none rounded-3xl duration-300 bg-gray-500 text-shadow"
+                        onClick={() => {
+                            setPokemonType(undefined);
+                        }}
+                    >
+                        Reset Filter
+                    </button>
+                </div>
+                {/* Ensures that there is enough space for the type filter container, so the elements positions below are not affected by it by the state. */}
+                <div
+                    className={`col-span-2 mb-8 transition-all duration-500 transform h-[252px] ${
+                        showTypeBox
+                            ? "opacity-100 scale-100 translate-y-0"
+                            : "opacity-0 scale-95 translate-y-4 pointer-events-none"
+                    }`}
+                >
+                    {showTypeBox && (
+                        <div className="grid grid-cols-6 border-4 rounded-xl border-black p-2 gap-2">
+                            {buttonTypes.map((buttonType) => {
+                                return (
+                                    <button
+                                        key={buttonType}
+                                        className={`${getBackgroundColor(
+                                            buttonType
+                                        )} p-4 rounded-lg font-bold text-xl text-shadow text-white`}
+                                        onClick={() => {
+                                            setPokemonType(buttonType);
+                                        }}
+                                    >
+                                        {buttonType}
+                                    </button>
+                                );
+                            })}
+                        </div>
+                    )}
+                </div>
+                <div>
+                    {spriteImage !== undefined && (
+                        <Image
+                            src={spriteImage || ""}
+                            alt="pokemon sprite"
+                            className="pixelated sticky top-0 w-full"
+                            width={96}
+                            height={96}
+                        />
+                    )}
+                </div>
+                <div id="pokemon-card-wrapper" className="col-span-2">
+                    {filteredList.map((pokemon) => {
+                        return (
+                            <PokemonCard
+                                key={pokemon.name}
+                                pokemon={pokemon}
+                                onMouseEnter={() => {
+                                    if (
+                                        pokemon.sprites.front_default === null
+                                    ) {
+                                        return;
+                                    }
+
+                                    setSpriteImage(
+                                        pokemon.sprites.front_default
+                                    );
+                                }}
+                                onMouseLeave={() => setSpriteImage(undefined)}
+                            />
+                        );
+                    })}
+                </div>
             </div>
         </Layout>
     );
