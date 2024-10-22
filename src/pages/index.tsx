f47892483095adacf15175fc2468b0932aca9a0d
Marvin <maples4000@googlemail.com>
2024-10-22 11:59:51 +0200

    add content to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import Layout from "~/components/Layout";
 import "~/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <Layout>
-            <p>Hier kurzen Beschreibungsparagraph einfügen</p>
+            <p className="text-3xl flex justify-center pt-2 font-semibold pb-24">
+                Hi, i`m Marvin! Or Acid, if you want! <br /> Feel free to
+                explore my first website!
+            </p>
+            <div className="grid grid-cols-2 gap-4 w-fit mx-auto">
+                <Link
+                    href="/pokedex-site"
+                    className="p-12 border border-black rounded-md bg-indigo-200 hover:bg-indigo-300 col-span-1 w-fit"
+                >
+                    Watch your Pokedex
+                </Link>
+            </div>
         </Layout>
     );
 }
