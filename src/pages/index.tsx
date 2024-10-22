0b0d264c922192161752e312565e4a052023dc79
Marvin <maples4000@googlemail.com>
2024-10-22 12:10:06 +0200

    center the text with text-center and delete unnecessary class name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 export default function Home() {
     return (
         <Layout>
-            <p className="text-3xl flex justify-center pt-2 font-semibold pb-24">
+            <p className="text-3xl text-center pt-2 font-semibold pb-24">
                 Hi, i`m Marvin! Or Acid, if you want! <br /> Feel free to
                 explore my first website!
             </p>
             <div className="grid grid-cols-2 gap-4 w-fit mx-auto">
                 <Link
                     href="/pokedex-site"
-                    className="p-12 border border-black rounded-md bg-indigo-200 hover:bg-indigo-300 col-span-1 w-fit"
+                    className="p-12 border border-black rounded-md bg-indigo-200 hover:bg-indigo-300 w-fit"
                 >
                     Watch your Pokedex
                 </Link>
