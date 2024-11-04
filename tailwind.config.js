1b89b2d68b7bbd191b87a76fa5f04aa166cf132b
Marvin <maples4000@googlemail.com>
2024-11-04 16:03:23 +0100

    restyle detail page and replace css with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,11 @@ module.exports = {
             },
             width: {
                 sidebar: "200px",
+                spriteimage: "250px",
             },
             height: {
                 header: "100px",
+                spriteimage: "250px",
             },
         },
         screens: {
@@ -51,6 +53,7 @@ module.exports = {
             steel: "rgb(143, 143, 143)",
             dark: "rgb(40, 40, 40)",
             fairy: "hotpink",
+            "white-op": "rgba(255, 255, 255, 0.2)",
         },
     },
     plugins: [],
