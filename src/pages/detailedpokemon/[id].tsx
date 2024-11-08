18771c19497a212907d6a931cff145a08592ba5e
Marvin <maples4000@googlemail.com>
2024-11-08 11:43:30 +0100

    make layout responsive for mobile viewport

--- src/pages/detailedpokemon/[id].tsx
+++ src/pages/detailedpokemon/[id].tsx
@@ -78,153 +78,152 @@ export default function DetailedPokemon() {
 
     return (
         <Layout>
-            <div className="grid desktop:grid-cols-12 mobile:grid-cols-4 gap-4">
-                <div className="flex gap-3.5">
-                    <div className="flex flex-col gap-3.5">
-                        <Image
-                            alt="Pokemon sprite"
-                            width="250"
-                            height="250"
-                            className="h-min-[250px] w-min-[250px] border-8 border-double border-black rounded-xl cursor-pointer bg-white/20"
-                            src={
-                                isShiny
-                                    ? pokemon.sprites.front_shiny!
-                                    : pokemon.sprites.front_default!
-                            }
-                            onClick={() => setIsShiny(!isShiny)}
-                        />
-                        {/* Base Stat Container */}
-                        <div className="flex flex-col gap-4 border-2 border-black rounded-lg p-4 bg-white/20">
-                            <span className="text-xl border-b-4 font-bold border-black">
-                                Base Stats:
-                            </span>
-                            <div className="grid grid-cols-2 gap-y-1">
-                                {pokemon.stats.map((stat) => {
-                                    return (
-                                        <Fragment key={stat.stat.name}>
-                                            <span className="text-lg">
-                                                {statMap.get(stat.stat.name)}
-                                            </span>
-                                            <span className="text-lg">
-                                                {stat.base_stat || "-"}
-                                            </span>
-                                        </Fragment>
-                                    );
-                                })}
-                            </div>
-                            <span className="text-xl font-bold">
-                                Total Base Stat: {totalStats}
-                            </span>
+            <div className="grid desktop:grid-cols-12 grid-cols-4 gap-4">
+                {/* left wrapper */}
+                <div className="grid gap-3.5 grid-cols-subgrid desktop:col-span-3 col-span-4 content-start">
+                    <Image
+                        alt="Pokemon sprite"
+                        width="96"
+                        height="96"
+                        className="h-min-[250px] pixelated w-full border-8 border-double border-black rounded-xl cursor-pointer bg-white/20 col-span-full"
+                        src={
+                            isShiny
+                                ? pokemon.sprites.front_shiny!
+                                : pokemon.sprites.front_default!
+                        }
+                        onClick={() => setIsShiny(!isShiny)}
+                    />
+                    {/* Base Stat Container */}
+                    <div className="flex flex-col gap-4 border-2 border-black rounded-lg p-4 bg-white/20 col-span-full">
+                        <span className="text-xl border-b-4 font-bold border-black">
+                            Base Stats:
+                        </span>
+                        <div className="grid grid-cols-2 gap-y-1">
+                            {pokemon.stats.map((stat) => {
+                                return (
+                                    <Fragment key={stat.stat.name}>
+                                        <span className="text-lg">
+                                            {statMap.get(stat.stat.name)}
+                                        </span>
+                                        <span className="text-lg">
+                                            {stat.base_stat || "-"}
+                                        </span>
+                                    </Fragment>
+                                );
+                            })}
                         </div>
-                        {/* Ability Container */}
-                        <div className="flex flex-col border-2 border-black rounded-lg p-4 bg-white/20">
-                            <span className="text-xl font-bold">
-                                Abilities:
-                            </span>
-                            <div className="flex flex-col">
-                                {pokemon.abilities.map((pokemonAbility) => {
-                                    const pokeAbility = abilities.find(
-                                        (ability) => {
-                                            return (
-                                                pokemonAbility.ability.name ===
-                                                ability.name
-                                            );
-                                        }
-                                    );
-                                    if (pokeAbility === undefined) {
-                                        return null;
-                                    }
-                                    const engAbilityName =
-                                        pokeAbility.names.find(
-                                            (abilityName) => {
-                                                return (
-                                                    abilityName.language
-                                                        .name === "en"
-                                                );
-                                            }
-                                        );
-                                    if (engAbilityName === undefined) {
-                                        return null;
-                                    }
-                                    if (pokemonAbility.is_hidden === false) {
-                                        return (
-                                            <span key={pokeAbility.name}>
-                                                {engAbilityName.name}
-                                            </span>
-                                        );
-                                    } else {
-                                        return (
-                                            <div
-                                                className="flex flex-col"
-                                                key={pokeAbility.name}
-                                            >
-                                                <span className="text-xl font-bold">
-                                                    Hidden Ability:
-                                                </span>
-                                                <span>
-                                                    {engAbilityName.name}
-                                                </span>
-                                            </div>
-                                        );
-                                    }
-                                })}
-                            </div>
-                        </div>
+                        <span className="text-xl font-bold">
+                            Total Base Stat: {totalStats}
+                        </span>
                     </div>
-                    <div className="flex w-full items-start flex-col gap-2.5">
-                        {/* Upper Detail Container */}
-                        <div className="flex border-2 border-black rounded-lg gap-8 items-center p-3.5 bg-white/20">
-                            <div className="flex flex-col gap-3.5">
-                                <div className="flex gap-3.5">
-                                    <span className="text-3xl font-bold">{`#${pokemon.id}`}</span>
-                                    <span className="text-3xl font-bold">
-                                        {pokemon.name}
-                                    </span>
-                                </div>
-                                <div>
-                                    <span className="text-3xl font-bold mt-1">
-                                        jap: {japname?.name || "-"}
-                                    </span>
-                                </div>
-                            </div>
-                            <div className="flex flex-col text-3xl gap-3.5">
-                                <span className="text-3xl font-bold">
-                                    habitat: {pokemonSpecies.habitat.name}{" "}
-                                </span>
-                                <span className="text-3xl font-bold">
-                                    existing since: Generation{" "}
-                                    {gen?.toUpperCase() || "-"}
-                                </span>
-                            </div>
-                            <div className="gap-3 flex flex-col">
-                                {pokemon.types.map((type) => {
+                    {/* Ability Container */}
+                    <div className="flex flex-col border-2 border-black rounded-lg p-4 bg-white/20 col-span-full">
+                        <span className="text-xl font-bold">Abilities:</span>
+                        <div className="flex flex-col">
+                            {pokemon.abilities.map((pokemonAbility) => {
+                                const pokeAbility = abilities.find(
+                                    (ability) => {
+                                        return (
+                                            pokemonAbility.ability.name ===
+                                            ability.name
+                                        );
+                                    }
+                                );
+                                if (pokeAbility === undefined) {
+                                    return null;
+                                }
+                                const engAbilityName = pokeAbility.names.find(
+                                    (abilityName) => {
+                                        return (
+                                            abilityName.language.name === "en"
+                                        );
+                                    }
+                                );
+                                if (engAbilityName === undefined) {
+                                    return null;
+                                }
+                                if (pokemonAbility.is_hidden === false) {
                                     return (
-                                        <span
-                                            className={`${getBackgroundColor(
-                                                type.type.name as Type
-                                            )} border-2 border-black rounded-lg p-1.5 text-3xl`}
-                                            key={type.type.name}
-                                        >
-                                            {type.type.name}
+                                        <span key={pokeAbility.name}>
+                                            {engAbilityName.name}
                                         </span>
                                     );
-                                })}
-                            </div>
-                            <button
-                                onClick={() => {
-                                    audioRef.current?.play();
-                                }}
-                            >
-                                <img
-                                    src="https://cdn-icons-png.flaticon.com/512/4028/4028535.png"
-                                    className="w-auto h-[100px]"
-                                />
-                                <audio
-                                    ref={audioRef}
-                                    src={pokemon.cries.legacy}
-                                ></audio>
-                            </button>
+                                } else {
+                                    return (
+                                        <div
+                                            className="flex flex-col"
+                                            key={pokeAbility.name}
+                                        >
+                                            <span className="text-xl font-bold">
+                                                Hidden Ability:
+                                            </span>
+                                            <span>{engAbilityName.name}</span>
+                                        </div>
+                                    );
+                                }
+                            })}
                         </div>
+                    </div>
+                </div>
+                {/* right wrapper */}
+                <div className="grid grid-cols-subgrid desktop:col-span-9 col-span-4 content-start">
+                    {/* Upper Detail Container */}
+                    <div className="flex desktop:flex-row flex-col gap-8 col-span-9 border-2 border-black rounded-lg p-3.5 bg-white/20">
+                        {/* name wrapper */}
+                        <div className="flex flex-col gap-3.5">
+                            <div className="flex gap-3.5">
+                                <span className="text-3xl font-bold">{`#${pokemon.id}`}</span>
+                                <span className="text-3xl font-bold">
+                                    {pokemon.name}
+                                </span>
+                            </div>
+                            <div>
+                                <span className="text-3xl font-bold mt-1">
+                                    jap: {japname?.name || "-"}
+                                </span>
+                            </div>
+                        </div>
+                        {/* habitat gen wrapper */}
+                        <div className="flex flex-col text-3xl gap-3.5">
+                            <span className="text-3xl font-bold">
+                                habitat: {pokemonSpecies.habitat.name}{" "}
+                            </span>
+                            <span className="text-3xl font-bold">
+                                existing since: Generation{" "}
+                                {gen?.toUpperCase() || "-"}
+                            </span>
+                        </div>
+                        {/* pokemon type wrapper */}
+                        <div className="gap-3 flex desktop:flex-col">
+                            {pokemon.types.map((type) => {
+                                return (
+                                    <span
+                                        className={`${getBackgroundColor(
+                                            type.type.name as Type
+                                        )} border-2 border-black rounded-lg p-1.5 text-3xl`}
+                                        key={type.type.name}
+                                    >
+                                        {type.type.name}
+                                    </span>
+                                );
+                            })}
+                        </div>
+                        <button
+                            onClick={() => {
+                                audioRef.current?.play();
+                            }}
+                        >
+                            <Image
+                                src="/PlayButton.png"
+                                alt="audio button"
+                                width={96}
+                                height={96}
+                            />
+                            <audio
+                                ref={audioRef}
+                                src={pokemon.cries.legacy}
+                            ></audio>
+                        </button>
                     </div>
                 </div>
             </div>
